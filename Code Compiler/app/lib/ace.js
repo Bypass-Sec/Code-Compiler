6c32cb4be03282140f77a2ab871a027f500dc9ce
Omer Abdalla <omer.sharif1998@gmail.com>
2025-02-21 20:41:29 +0300

    Update ace.js

--- Code Compiler/app/lib/ace.js
+++ Code Compiler/app/lib/ace.js
@@ -1,1 +1,76 @@
+(function() {
+
+var ACE_NAMESPACE = "";
+
+var global = (function() { return this; })();
+if (!global && typeof window != "undefined") global = window; // strict mode
+
+
+if (!ACE_NAMESPACE && typeof requirejs !== "undefined")
+    return;
+var define = function(module, deps, payload) {
+    if (typeof module !== "string") {
+        if (define.original)
+            define.original.apply(this, arguments);
+        else {
+            console.error("dropping module because define wasn\'t a string.");
+            console.trace();
+        }
+        return;
+    }
+    if (arguments.length == 2)
+        payload = deps;
+    if (!define.modules[module]) {
+        define.payloads[module] = payload;
+        define.modules[module] = null;
+    }
+};
+
+define.modules = {};
+define.payloads = {};
+
+var _require = function(parentId, module, callback) {
+    if (typeof module === "string") {
+        var payload = lookup(parentId, module);
+        if (payload != undefined) {
+            callback && callback();
+            return payload;
+        }
+    } else if (Object.prototype.toString.call(module) === "[object Array]") {
+        var params = [];
+        for (var i = 0, l = module.length; i < l; ++i) {
+            var dep = lookup(parentId, module[i]);
+            if (dep == undefined && require.original)
+                return;
+            params.push(dep);
+        }
+        return callback && callback.apply(null, params) || true;
+    }
+};
+
+var require = function(module, callback) {
+    var packagedModule = _require("", module, callback);
+    if (packagedModule == undefined && require.original)
+        return require.original.apply(this, arguments);
+    return packagedModule;
+};
+
+var normalizeModule = function(parentId, moduleName) {
+    // normalize plugin requires
+    if (moduleName.indexOf("!") !== -1) {
+        var chunks = moduleName.split("!");
+        return normalizeModule(parentId, chunks[0]) + "!" + normalizeModule(parentId, chunks[1]);
+    }
+   
+    if (moduleName.charAt(0) == ".") {
+        var base = parentId.split("/").slice(0, -1).join("/");
+        moduleName = base + "/" + moduleName;
+
+        while(moduleName.indexOf(".") !== -1 && previous != moduleName) {
+            var previous = moduleName;
+            moduleName = moduleName.replace(/\/\.\//, "/").replace(/[^\/]+\/\.\.\//, "");
+        }
+    }
+    return moduleName;
+};
 
